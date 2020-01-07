95549c83861b79cf3b8a526fb06320a715369f11
destro014 <japmc11@gmail.com>
2020-01-07 15:10:17 +0545

    Update register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,65 @@
 <template>
   <div class="register shifted">
-    <h1>Register</h1>
+    <div class="container pt-lg-md">
+      <div class="row justify-content-center">
+        <div class="col-lg-5">
+          <div class="card shadow border-0">
+            <div class="card-body px-lg-5 py-lg-5">
+              <div class="text-center text-muted mb-4">
+                <h1>Register</h1>
+              </div>
+              <form>
+                <div class="form-group mb-3">
+                  <div class="input-group input-group-alternative">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <font-awesome-icon :icon="['fas', 'envelope']" />
+                      </span>
+                    </div>
+                    <input class="form-control" placeholder="Name" type="name" />
+                  </div>
+                </div>
+                <div class="form-group mb-3">
+                  <div class="input-group input-group-alternative">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <font-awesome-icon :icon="['fas', 'envelope']" />
+                      </span>
+                    </div>
+                    <input class="form-control" placeholder="Email" type="email" />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group input-group-alternative">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <font-awesome-icon :icon="['fas', 'lock']" />
+                      </span>
+                    </div>
+                    <input class="form-control" placeholder="Password" type="password" />
+                  </div>
+                </div>
+                <div class="text-center">
+                  <button type="button" class="btn btn-primary my-4">Register</button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col-6">
+              <a href="#" class="text-light">
+                <small>Forgot password?</small>
+              </a>
+            </div>
+            <div class="col-6 text-right">
+              <router-link :to="{name :'login'}">
+                <small>Login</small>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,4 +69,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.register {
+  .card {
+    background: $color-tertary;
+  }
+}
+</style>

3cb3f54b8cff377a6c875964ea912189b9f26ecc
destro014 <japmc11@gmail.com>
2020-01-09 22:43:59 +0545

    add product details page

--- src/router/index.js
+++ src/router/index.js
@@ -74,6 +74,18 @@ const routes = [{
     meta: {
       title: 'Cybershop | All Categories'
     }
+  },
+  {
+    path: "/product/:id/:name",
+    name: "product",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import( /* webpackChunkName: "about" */ "../views/ProductDetails.vue"),
+    meta: {
+      // title: $route.params.name
+    }
   }
 ];
 

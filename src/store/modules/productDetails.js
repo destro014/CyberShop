e088d130a8537abb8fbbf57e6899996a81e734a5
destro014 <japmc11@gmail.com>
2020-01-12 15:33:38 +0545

    Store module to display product details

---
+++ src/store/modules/productDetails.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const state = {
+    // endpoint: "https://internshopapi.000webhostapp.com/api/product"
+    endpoint: "../../categories.json"
+};
+const getters = {};
+const actions = {
+
+};
+const mutations = {
+
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations,
+}

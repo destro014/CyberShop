d8a28fadf1663c047c3216022f257e7263c534fb
destro014 <japmc11@gmail.com>
2020-01-12 15:37:38 +0545

    Add columns to display data

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="product-details shifted" v-if="product">
-    <h1>This is the product details of {{name}}</h1>
-    {{id}}
+    <!-- {{id}}
     <br />
     {{product.title}}
     <br />
@@ -15,7 +14,24 @@
     <br />
     {{product.title}}
     <br />
-    <progressive-img :src="product.image" :placeholder="this.image" :blur="0" />
+    <progressive-img :src="product.image" :placeholder="this.image" :blur="0" />-->
+    <div class="row">
+      <div class="col-lg-6">
+        <progressive-img :src="product.image" :placeholder="this.image" :blur="0" />
+      </div>
+      <div class="col-lg-6">
+        <ul>
+          <li>
+            <h5 class="display-4">{{product.title}}</h5>
+          </li>
+          <li>{{product.type}}</li>
+          <li>{{product.description}}</li>
+          <li>{{product.price}}</li>
+          <li>{{product.price}}</li>
+          <li>{{product.title}}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,5 +59,11 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.product-details {
+  padding-top: 100px;
+  h5 {
+    margin-bottom: 0;
+  }
+}
 </style>

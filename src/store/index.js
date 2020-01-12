cbf5033a33ac06e75818da832687ede0e4afa141
destro014 <japmc11@gmail.com>
2020-01-12 15:32:41 +0545

    Add product details and product modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import categories from './modules/categories';
+import productDetails from './modules/productDetails';
+import products from './modules/products';
 
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    categories
+    categories,
+    productDetails,
+    products
   }
 });

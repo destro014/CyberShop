19458e0bc7b765af6189a88d05d26e71549cf1e6
destro014 <japmc11@gmail.com>
2020-01-13 21:30:44 +0545

    Added api call for users

--- src/store/modules/productDetails.js
+++ src/store/modules/productDetails.js
@@ -1,14 +1,42 @@
 import axios from 'axios'
+import router from '@/router'
 
 const state = {
-    // endpoint: "https://internshopapi.000webhostapp.com/api/product"
-    endpoint: "../../categories.json"
+    product: null,
+    seller: null,
+    id: router.currentRoute.params.id,
+    // endpoint: 'http://internshopapi.000webhostapp.com/api/products'
+    endpoint: "../../products.json"
 };
-const getters = {};
+const getters = {
+    product: state => state.product,
+    seller: state => state.seller
+};
 const actions = {
-
+    async fetchProduct({
+        commit
+    }, id) {
+        // const response = await axios.get(`${state.endpoint}/${id}`);
+        const response = await axios.get(state.endpoint);
+        commit('setProduct', response.data
+            .filter(obj => {
+                if (obj.product_identifier == id) {
+                    return obj;
+                }
+            })
+        );
+    },
+    async fetchSeller({
+        commit
+    },sellerId) {
+        console.log(this.state.product.seller)
+        const response = await axios.get(`$(http://internshopapi.000webhostapp.com/api/users/${sellerId})`);
+        commit('setUser', response.data)
+    }
 };
 const mutations = {
+    setProduct: (state, product) => (state.product = product[0]),
+    setUser: (state, user) => (state.seller = user)
 
 };
 

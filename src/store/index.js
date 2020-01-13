bc1453375c4bc0ae412ba8e7e5368bb440f0ef46
destro014 <japmc11@gmail.com>
2020-01-13 10:49:50 +0545

    import products modules

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,12 @@ import categories from './modules/categories';
 import productDetails from './modules/productDetails';
 import products from './modules/products';
 
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     categories,
     productDetails,
-    products
+    products,
   }
 });

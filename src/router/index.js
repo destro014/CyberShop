04862e8722b25b1739feb5a2e8c5128c71082b38
destro014 <japmc11@gmail.com>
2020-01-19 14:26:45 +0545

    Add category route

--- src/router/index.js
+++ src/router/index.js
@@ -86,6 +86,18 @@ const routes = [{
     meta: {
       title: 'Buy in Cybershop'
     }
+  },
+  {
+    path: "/category/:id/:name",
+    name: "category",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import( /* webpackChunkName: "about" */ "../views/Category.vue"),
+    meta: {
+      title: 'Buy in Cybershop'
+    }
   }
 ];
 

ea833367be493c61eb47dfe6dec3b6a8caa19451
destro014 <japmc11@gmail.com>
2020-01-19 14:26:21 +0545

    Update router link

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="category-card">
-    <router-link :to="{name : category.link}">
+  <div class="category-card" v-if="category">
+    <router-link :to="{name : 'category', params: {name: category.title, id: category.id}}">
       <div class="card shadow--hover border-0">
         <div class="icon">
           <font-awesome-icon :icon="['fas',category.icon ]" />
         </div>
-        <div class="category">{{category.title}}</div>
+        <div class="category-name">{{category.title}}</div>
       </div>
     </router-link>
   </div>
